c3559aa2af79ea730c9cc5d58fa90cd136ebb3b5
Ruedi Steinmann <ruediste@gmail.com>
2014-07-26 08:52:31 +0200

    fix

--- docker/pullImages.sh
+++ docker/pullImages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # pull all container images
-for dir in `find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n`
+for dir in `find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n'`
 do
   sudo docker pull localhost:5000/infra_$dir
 done
