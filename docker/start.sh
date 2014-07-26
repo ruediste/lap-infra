abbf35ade5fc99d65d9888c93975ec6842c75924
Ruedi Steinmann <ruediste@gmail.com>
2014-07-26 16:18:00 +0200

    fix

--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,7 @@ if [ -z "$LAP_ENVIRONMENT" ]; then
     exit 1
 fi  
 
-sudo docker run --name infra_environment -v /data/lap-infra/env/$LAP_ENVIRONMENT:/env localhost:5000/infra_environment
+sudo docker run --name infra_environment -v /data/lap-infra/env/$LAP_ENVIRONMENT:/env localhost:5000/infra_environment echo foo
 
 sudo docker run -d --volumes-from infra_environment --name infra_gitolite -v /data/gitolite:/var/lib/gitolite -p 2200:22 localhost:5000/infra_gitolite
 sudo docker run -d --volumes-from infra_environment --name infra_registry -v /data/registry:/data -p 5000:5000 -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/data localhost:5000/infra_registry
