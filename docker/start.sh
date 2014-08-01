3857b4725d7dfdb7db5bd121a9e0c650509734f5
Ruedi Steinmann <ruediste@gmail.com>
2014-08-01 20:44:29 +0200

    add go server docker

--- docker/start.sh
+++ docker/start.sh
@@ -9,5 +9,6 @@ sudo docker run --name infra_environment -v /data/lap-infra/env/$LAP_ENVIRONMENT
 
 sudo docker run -d --volumes-from infra_environment --name infra_gitolite -v /data/gitolite:/var/lib/gitolite -p 2200:22 localhost:5000/infra_gitolite
 sudo docker run -d --volumes-from infra_environment --name infra_registry -v /data/registry:/data -p 5000:5000 -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/data localhost:5000/infra_registry
-sudo docker run -d --volumes-from infra_environment --name infra_registry -p `cat /etc/primaryip`:53:53/udp -p `cat /etc/primaryip`:53:53 -i -t --name infra_dns localhost:5000/infra_dns /run.sh `cat /etc/primaryip`
+sudo docker run -d --volumes-from infra_environment --name infra_dns -p `cat /etc/primaryip`:53:53/udp -p `cat /etc/primaryip`:53:53 localhost:5000/infra_dns /run.sh `cat /etc/primaryip`
 ./doStartRedmine.sh 
+sudo docker run -d --volumes-from infra_environment --name infra_go_server -p 8153:8153 localhost:5000/infra_go_server /bin/bash -c "service go-server start; tail -f /var/log/go-server/*"
